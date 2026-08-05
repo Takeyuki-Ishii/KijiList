f78ceebe7d399ec2fb8197e34b2dd724dd011835
Takeyuki-Ishii <t141ishii@gmail.com>
2026-08-05 14:13:48 +0900

    ・メモ項目の追加 ・作成日時、更新日時の項目追加

--- src/main/java/com/example/KijiList/Task.java
+++ src/main/java/com/example/KijiList/Task.java
@@ -1,15 +1,19 @@
 package com.example.KijiList;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Task {
 
     @Id
@@ -22,7 +26,7 @@ public class Task {
 
     // ★期限：StringからLocalDateに変更し、未入力禁止 ＋ 今日以降のみ許可
     @NotNull(message = "期限を入力してください")
-    // @FutureOrPresent(message = "期限には今日以降の日付を指定してください")
+    @FutureOrPresent(message = "期限には今日以降の日付を指定してください")
     private LocalDate deadline;
 
     // 完了フラグ（初期値は未完了の false）
@@ -30,6 +34,18 @@ public class Task {
 
     // 重要度（"高", "中", "低" のいずれかの文字列が入る）
     private String priority;
+
+    @Column(columnDefinition = "TEXT") // 長文に対応させる設定
+    @Size(max = 100, message = "メモは100文字以内で入力してください")
+    private String memo;
+
+    @CreatedDate // 作成時に自動挿入
+    @Column(updatable = false, nullable = false) // 更新時には上書きさせない
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 更新時に自動挿入
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
     public Task() {
     }
@@ -67,6 +83,13 @@ public class Task {
     public void setPriority(String priority) {
         this.priority = priority;
     }
+
+    public String getMemo() { return memo; }
+    public void setMemo(String memo) { this.memo = memo; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
 
     // ★ 動的に期限切れを判定するメソッド
     public boolean isOverdue() {
--- src/main/java/com/example/KijiList/Test06Application.java
+++ src/main/java/com/example/KijiList/Test06Application.java
@@ -2,8 +2,10 @@ package com.example.KijiList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication // ★これが消えていたため、エラーになっていました！
+@EnableJpaAuditing // 追加
+@SpringBootApplication //
 public class Test06Application {
 
 	public static void main(String[] args) {
